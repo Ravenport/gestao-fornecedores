d545b935cccb0c5e079a3c503d0e90272b464bf1
unknown <vp22756@gmail.com>
2025-08-24 23:49:40 -0300

    first commit

---
+++ src/test/java/com/projeto/bloco/gestao_fornecedores/GestaoFornecedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.bloco.gestao_fornecedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFornecedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
